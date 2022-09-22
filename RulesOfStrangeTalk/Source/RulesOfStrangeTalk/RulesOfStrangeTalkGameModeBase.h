7a3e52d0631a140b5b5304964ea01f988436ab6f
Meteorite <meteoriteeric@outlook.com>
2022-09-23 00:01:56 +0800

    Add some basic function
    Add some basic function

---
+++ RulesOfStrangeTalk/Source/RulesOfStrangeTalk/RulesOfStrangeTalkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RulesOfStrangeTalkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RULESOFSTRANGETALK_API ARulesOfStrangeTalkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
